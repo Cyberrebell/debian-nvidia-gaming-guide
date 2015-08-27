bd8fe3a20bab63ac149d3909e6913f29272c3c3d
stephan <stephan@cyberrebell>
2015-08-27 21:47:58 +0200

    added new control-display parameter for nvidia-settings...

--- gpu_fan_target.sh
+++ gpu_fan_target.sh
@@ -16,19 +16,19 @@ min_fanspeed=20
 
 while true
 do
-	current_temperature=`nvidia-settings -t -q [GPU:0]/GPUCoreTemp`
-	current_fanspeed=`nvidia-settings -t -q [fan:0]/GPUCurrentFanSpeed`
+	current_temperature=`nvidia-settings -t -c :0 -q [GPU:0]/GPUCoreTemp`
+	current_fanspeed=`nvidia-settings -t -c :0 -q [fan:0]/GPUCurrentFanSpeed`
 	current_interval=$interval
 		
 	if [ $current_temperature -gt `expr $target_temperature + $target_temperature_range` ]; then
 		current_fanspeed=`expr $current_fanspeed + $adjustment_rate`
-		nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=$current_fanspeed
+		nvidia-settings -c :0 -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=$current_fanspeed
 		current_interval=$adjustment_rate
 	else
 		if [ $current_temperature -lt `expr $target_temperature - $target_temperature_range` ]; then
 			if [ $current_fanspeed -gt `expr $min_fanspeed` ]; then
 				current_fanspeed=`expr $current_fanspeed - $adjustment_rate`
-				nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=$current_fanspeed
+				nvidia-settings -c :0 -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=$current_fanspeed
 				current_interval=$adjustment_rate
 			fi
 		fi
